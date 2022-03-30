ac72e6a843afe52875500055cb9fb71d58114eb0
Varma2324 <101701117+Varma2324@users.noreply.github.com>
2022-03-31 00:03:14 +0530

    Update testing.c

--- MiniProject_C/3_Implementation/test/testing.c
+++ MiniProject_C/3_Implementation/test/testing.c
@@ -1,1 +1,12 @@
+#include "unity.h"
+#include "RESERVETICKET.h"
+#include "BUSAVAILABILITY.h"
+#include "SEATBOOKING.h"
+#include "DISPLAYTICKET.h"
 
+#define Online_Bus_Ticket_Reservation_System
+
+void test_reserveaticket(void);
+void test_busavailability(void);
+void test_bookingseats(void);
+void test_displaystickets(void);
