914ead2856f52ae46b8e47ae23bc0e53455fb595
Varma2324 <101701117+Varma2324@users.noreply.github.com>
2022-03-31 00:21:03 +0530

    Update testing.c

--- MiniProject_C/3_Implementation/test/testing.c
+++ MiniProject_C/3_Implementation/test/testing.c
@@ -1,12 +1,51 @@
 #include "unity.h"
-#include "RESERVETICKET.h"
-#include "BUSAVAILABILITY.h"
-#include "SEATBOOKING.h"
-#include "DISPLAYTICKET.h"
+#include "RESERVATION.h"
+#include "VIEWDETAILS.h"
+#include "SPECIFICBUS.h"
+#include "CHARGE.h"
+#include "DISPLAYSTICKET.h"
 
 #define Online_Bus_Ticket_Reservation_System
 
-void test_reserveaticket(void);
-void test_busavailability(void);
-void test_bookingseats(void);
-void test_displaystickets(void);
+void test_reservation(void);
+void test_viewdetails(void);
+void test_specificbus(void);
+void test_charge(void);
+void test_displaysticket(void);
+
+void setUp(){}
+void tearDown(){}
+
+int main()
+{
+UNITY_BEGIN();
+  
+RUN_TEST(test_reservation);
+RUN_TEST(test_viewdetails);
+RUN_TEST(test_specificbus);
+RUN_TEST(test_charge);  
+RUN_TEST(test_displaysticket);
+
+return UNITY_END();
+}
+
+void test_reservation(void){
+TEST_ASSERT_EQUAL(1,reserveaticket());
+}
+
+void test_viewdetails(void){
+TEST_ASSERT_EQUAL(1,busavailability());
+}
+
+void test_specificbus(void){
+TEST_ASSERT_EQUAL(1,seatbooking());
+}
+
+void test_charge(void){
+TEST_ASSERT_EQUAL(1,payment());
+  
+void test_displaysticket(void){
+TEST_ASSERT_EQUAL(1,ticketbookingdetails());  
+  
+  
+}
