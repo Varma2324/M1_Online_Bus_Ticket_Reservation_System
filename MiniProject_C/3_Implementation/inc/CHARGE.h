e7df15560eb215e6f71d6681a61c4892c1b66ae2
Varma2324 <101701117+Varma2324@users.noreply.github.com>
2022-03-31 15:49:36 +0530

    Create CHARGE.h

---
+++ MiniProject_C/3_Implementation/inc/CHARGE.h
@@ -0,0 +1,45 @@
+float charge(int bus_num,int num_of_seats)
+{
+
+	if (bus_num==1001)
+	
+	{
+		return(500*num_of_seats);
+	}
+	if (bus_num==1002)
+	{
+		return(500*num_of_seats);
+	}
+	if (bus_num==1003)
+	{
+		return(450*num_of_seats);
+	}
+	if (bus_num==1004)
+	{
+		return(450*num_of_seats);
+	}
+	if (bus_num==1005)
+	{
+		return(400*num_of_seats);
+	}
+	if (bus_num==1006)
+	{
+		return(400*num_of_seats);
+	}
+	if (bus_num==1007)
+	{
+		return(350*num_of_seats);
+	}
+	if (bus_num==1008)
+	{
+		return(350*num_of_seats);
+	}
+	if (bus_num==1009)
+	{
+		return(600*num_of_seats);
+	}
+	if (bus_num==1010)
+	{
+		return(600*num_of_seats);
+	}
+}
