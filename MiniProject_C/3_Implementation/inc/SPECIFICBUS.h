837754e40713b1de388ebac99d3ef6b7c4675dde
Varma2324 <101701117+Varma2324@users.noreply.github.com>
2022-03-31 15:44:47 +0530

    Update SPECIFICBUS.h

--- MiniProject_C/3_Implementation/inc/SPECIFICBUS.h
+++ MiniProject_C/3_Implementation/inc/SPECIFICBUS.h
@@ -1,64 +1,73 @@
+/* specificbus function*/
 void specificbus(int bus_num)
 {
 
-	if (bus_num==1001)
+	if (bus_num==1001)  /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1001*/
 	{
-		printf("\nbus:\t\t\tGodavari Travels ");
-		printf("\nDestination:\t\tDharan to Kavre");
-		printf("\nDeparture:\t\t9am ");
+		printf("\nbus:\t\t\tGodavari Travels ");     /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tDharan to Kavre"); /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t9am ");              /* it display the bus timing to the user */
 	}
-	if (bus_num==1002)
-	{
-		printf("\nbus:\t\t\tDevit Travels ");
-		printf("\nDestination:\t\tKavre to Dharan");
-		printf("\nDeparture:\t\t12pm");
+	if (bus_num==1002) /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1002*/
+	{ 
+	      	printf("\nbus:\t\t\tDevit Travels ");          /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tKavre to Dharan");   /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t12pm");                /* it display the bus timing to the user */
 	}
-	if (bus_num==1003)
+	if (bus_num==1003)   /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1003*/
 	{
-		printf("\nbus:\t\t\tHero Travels ");
-		printf("\nDestination:\t\tBenighat to Pokhara");
-		printf("\nDeparture:\t\t8am");
+		printf("\nbus:\t\t\tHero Travels ");                 /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tBenighat to Pokhara");     /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t8am");                       /* it display the bus timing to the user */
 	}
-	if (bus_num==1004)
+	if (bus_num==1004)   /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1004*/
 	{
-		printf("\nbus:\t\t\tSuper Deluxe ");
-		printf("\nDestination:\t\tPokhara to Benighat");
-		printf("\nDeparture:\t\t11am ");
+		printf("\nbus:\t\t\tSuper Deluxe ");                 /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tPokhara to Benighat");     /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t11am ");                     /* it display the bus timing to the user */
+
 	}
-	if (bus_num==1005)
+	if (bus_num==1005)  /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1005*/
 	{
-		printf("\nbus:\t\t\tSai Baba Travels ");
-		printf("\nDestination:\t\tMaitidevi to Janakpur");
-		printf("\nDeparture:\t\t7am");
+		printf("\nbus:\t\t\tSai Baba Travels ");             /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tMaitidevi to Janakpur");   /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t7am");                       /* it display the bus timing to the user */
 	}
-	if (bus_num==1006)
+	if (bus_num==1006)  /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1006*/
 	{
-		printf("\nbus:\t\t\tShine On Travels ");
-		printf("\nDestination:\t\tJanakpur to Maitidevi ");
-		printf("\nDeparture:\t\t9.30am ");
+		printf("\nbus:\t\t\tShine On Travels ");             /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tJanakpur to Maitidevi ");  /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t9.30am ");                   /* it display the bus timing to the user */
 	}
-	if (bus_num==1007)
+	if (bus_num==1007)   /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1007*/
 	{
-		printf("\nbus:\t\t\tMayur Travels");
+		printf("\nbus:\t\t\tMayur Travels");           
+		                                                /* it displays the bus name to the user*/ 
+                                                               /* it displays the destination point to the user */
+                                                               /* it display the bus timing to the user */
 		printf("\nDestination:\t\tHumla toJumla ");
 		printf("\nDeparture:\t\t1pm ");
-	}
-	if (bus_num==1008)
+	} 
+	if (bus_num==1008)  /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1008*/
 	{
+		/* it displays the bus name to the user*/ 
+                /* it displays the destination point to the user */
+                /* it display the bus timing to the user */
 		printf("\nbus:\t\t\tShree Travels ");
 		printf("\n Destination:\t\tJumla to Humla");
 		printf("\nDeparture:\t\t4pm ");
 	}
-	if (bus_num==1009)
+	if (bus_num==1009)   /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1009*/
 	{
-		printf("\nbus:\t\t\tKathmandu Express");
-		printf("\nDestination:\t\tKathmandu to Pokhara");
-		printf("\nDeparture:\t\t3.35pm ");
+		printf("\nbus:\t\t\tKathmandu Express");               /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tKathmandu to Pokhara");      /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t3.35pm ");                     /* it display the bus timing to the user */
 	}
-	if (bus_num==1010)
+	if (bus_num==1010)   /*@param bus_num takes the input and check the condition.if bus number is equal to the existing number then it displays the bus details of 1010*/
 	{
-		printf("\nbus:\t\t\tSajha Yatayat");
-		printf("\nDestination:\t\tPokhara to Kathmandu");
-		printf("\nDeparture:\t\t1.15 ");
+		printf("\nbus:\t\t\tSajha Yatayat");                   /* it displays the bus name to the user*/ 
+		printf("\nDestination:\t\tPokhara to Kathmandu");      /* it displays the destination point to the user */
+		printf("\nDeparture:\t\t1.15 ");                       /* it display the bus timing to the user */
 	}
+
 }
