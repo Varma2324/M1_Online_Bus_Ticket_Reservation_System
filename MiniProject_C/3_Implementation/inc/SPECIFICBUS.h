73d36f35ea17a4182c82757551b8aa8763d728a7
Varma2324 <101701117+Varma2324@users.noreply.github.com>
2022-03-31 15:22:10 +0530

    Create SPECIFICBUS.h

---
+++ MiniProject_C/3_Implementation/inc/SPECIFICBUS.h
@@ -0,0 +1,64 @@
+void specificbus(int bus_num)
+{
+
+	if (bus_num==1001)
+	{
+		printf("\nbus:\t\t\tGodavari Travels ");
+		printf("\nDestination:\t\tDharan to Kavre");
+		printf("\nDeparture:\t\t9am ");
+	}
+	if (bus_num==1002)
+	{
+		printf("\nbus:\t\t\tDevit Travels ");
+		printf("\nDestination:\t\tKavre to Dharan");
+		printf("\nDeparture:\t\t12pm");
+	}
+	if (bus_num==1003)
+	{
+		printf("\nbus:\t\t\tHero Travels ");
+		printf("\nDestination:\t\tBenighat to Pokhara");
+		printf("\nDeparture:\t\t8am");
+	}
+	if (bus_num==1004)
+	{
+		printf("\nbus:\t\t\tSuper Deluxe ");
+		printf("\nDestination:\t\tPokhara to Benighat");
+		printf("\nDeparture:\t\t11am ");
+	}
+	if (bus_num==1005)
+	{
+		printf("\nbus:\t\t\tSai Baba Travels ");
+		printf("\nDestination:\t\tMaitidevi to Janakpur");
+		printf("\nDeparture:\t\t7am");
+	}
+	if (bus_num==1006)
+	{
+		printf("\nbus:\t\t\tShine On Travels ");
+		printf("\nDestination:\t\tJanakpur to Maitidevi ");
+		printf("\nDeparture:\t\t9.30am ");
+	}
+	if (bus_num==1007)
+	{
+		printf("\nbus:\t\t\tMayur Travels");
+		printf("\nDestination:\t\tHumla toJumla ");
+		printf("\nDeparture:\t\t1pm ");
+	}
+	if (bus_num==1008)
+	{
+		printf("\nbus:\t\t\tShree Travels ");
+		printf("\n Destination:\t\tJumla to Humla");
+		printf("\nDeparture:\t\t4pm ");
+	}
+	if (bus_num==1009)
+	{
+		printf("\nbus:\t\t\tKathmandu Express");
+		printf("\nDestination:\t\tKathmandu to Pokhara");
+		printf("\nDeparture:\t\t3.35pm ");
+	}
+	if (bus_num==1010)
+	{
+		printf("\nbus:\t\t\tSajha Yatayat");
+		printf("\nDestination:\t\tPokhara to Kathmandu");
+		printf("\nDeparture:\t\t1.15 ");
+	}
+}
